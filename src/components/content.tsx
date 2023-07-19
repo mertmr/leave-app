7b7605182dd86f9bfcaf075f84579add1fc3486f
Mert Meral <mertmr@gmail.com>
2023-07-19 08:06:13 +0000

    Add dashboard to home page

---
+++ src/components/content.tsx
@@ -0,0 +1,274 @@
+import * as React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import { useUser } from "@clerk/nextjs";
+
+function Content() {
+  const user = useUser();
+  
+  return (
+    <div>
+      <div className="p-4 sm:ml-64">
+        <div className="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
+          <div className="bg-white-50 flex h-24 flex-col rounded dark:bg-gray-800">
+            <div className="text-black-400 text-2xl dark:text-gray-500">
+              Hello {user.user?.emailAddresses[0]?.emailAddress}
+            </div>
+            <div className="text-1xl text-gray-400 dark:text-gray-500">
+              Today is 19/07/2023
+            </div>
+          </div>
+          <div className="mb-4 grid grid-cols-3 gap-4">
+            <div>
+              <Card className="bg-gray-50 dark:bg-gray-800">
+                <CardHeader>
+                  <CardTitle className="text-black-400 dark:text-black-500">
+                    Total Leave
+                  </CardTitle>
+                  <CardDescription className="dark:text-black-500 text-gray-400">
+                    Your total leave remaining
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>16 Days</p>
+                </CardContent>
+              </Card>
+            </div>
+            <div>
+              <Card className="bg-gray-50 dark:bg-gray-800">
+                <CardHeader>
+                  <CardTitle className="text-black-400 dark:text-black-500">
+                    Total Leave / Hours
+                  </CardTitle>
+                  <CardDescription className="text-gray-400 dark:text-gray-500">
+                    Your total leave remaining in hours
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>16 Days</p>
+                </CardContent>
+              </Card>
+            </div>
+            <div>
+              <Card className="bg-gray-50 dark:bg-gray-800">
+                <CardHeader>
+                  <CardTitle className="text-black-400 dark:text-black-500">
+                    Total Leave / Hours
+                  </CardTitle>
+                  <CardDescription className="text-gray-400 dark:text-gray-500">
+                    Your total leave remaining in hours
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>16 Days</p>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+          <div className="mb-4 flex h-48 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+            <p className="text-2xl text-gray-400 dark:text-gray-500">
+              <svg
+                className="h-3.5 w-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+            </p>
+          </div>
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+          </div>
+          <div className="mb-4 flex h-48 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+            <p className="text-2xl text-gray-400 dark:text-gray-500">
+              <svg
+                className="h-3.5 w-3.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex h-28 items-center justify-center rounded bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="h-3.5 w-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Content };
