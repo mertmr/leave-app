23d28044072e94df5a1b1ed0a9e71ab546b22f90
Mert Meral <mertmr@gmail.com>
2023-07-19 12:13:57 +0000

    dark mode is broken, fix it

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -17,14 +17,14 @@ function Content() {
       <div className="p-4 sm:ml-64">
         <div className="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
           <div className="bg-white-50 flex h-24 flex-col rounded dark:bg-gray-800 mb-4 p-6">
-            <div className="text-black-400 text-2xl dark:text-white-500">
+            <div className="text-black-400 font-semibold text-lg md:text-2xl dark:text-white-500">
               Hello {user.user?.emailAddresses[0]?.emailAddress}
             </div>
-            <div className="text-1xl text-gray-400 dark:text-gray-500">
+            <div className="text-sm text-gray-400 dark:text-gray-500">
               Today is 19/07/2023
             </div>
           </div>
-          <div className="mb-4 grid grid-cols-3 gap-4">
+          <div className="mb-4 grid grid-cols-1 md:grid-cols-3 gap-4">
             <div>
               <Card className="bg-gray-50 dark:bg-gray-800">
                 <CardHeader>
@@ -32,7 +32,7 @@ function Content() {
                     Total Leave
                   </CardTitle>
                   <CardDescription className="dark:text-black-500 text-gray-400">
-                    Your total leave remaining
+                    Your total leave remaining in days
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
@@ -44,7 +44,7 @@ function Content() {
               <Card className="bg-gray-50 dark:bg-gray-800">
                 <CardHeader>
                   <CardTitle className="text-black-400 dark:text-black-500">
-                    Total Leave / Hours
+                    Total Leave
                   </CardTitle>
                   <CardDescription className="text-gray-400 dark:text-gray-500">
                     Your total leave remaining in hours
