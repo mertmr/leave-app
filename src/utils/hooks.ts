53c3b8ca8f916b39276053c28ef06677b2032955
Mert Meral <mertmr@gmail.com>
2023-07-19 11:57:57 +0000

    sidebar can be closed by clicking outside

---
+++ src/utils/hooks.ts
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect, RefObject } from "react";
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+export function useOutsideAlerter(ref: RefObject<HTMLDivElement>) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        alert("You clicked outside of me!");
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
